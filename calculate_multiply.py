f53dbf9d2d28f5377d2a58c54db7eab72fc0a754
Aishwarya Dhar <dhar.aishwarya@gmail.com>
2025-06-25 15:22:21 +0200

    multiply3

--- calculate_multiply.py
+++ calculate_multiply.py
@@ -1,12 +1,12 @@
 import numpy as np
 
-def calculate_multiply(data1,data2):
+def calculate_multiply(data1,data2,data3):
 """
 This function computes the mean of an array
 Input: One dimensional array
 Output: The mean of the array
 """	
-	x = data1*data2
+	x = data1*data2*data3
 	return x 
 
 
