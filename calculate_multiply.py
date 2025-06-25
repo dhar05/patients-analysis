71a65bb0928090440f82f13c3113ed6878297bbd
Aishwarya Dhar <dhar.aishwarya@gmail.com>
2025-06-25 15:15:34 +0200

    multiply

---
+++ calculate_multiply.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+def calculate_multiply(data1,data2):
+"""
+This function computes the mean of an array
+Input: One dimensional array
+Output: The mean of the array
+"""	
+	x = data1*data2
+	return x 
+
+
+
